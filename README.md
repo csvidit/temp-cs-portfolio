6fa509af09d1bb3b0b4a62899796d4160371839f
Vidit Khandelwal <viditk17@gmail.com>
2023-02-24 18:14:02 -0500

    update Card and README, add OpenGraph

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # temp-cs-portfolio
+
+A temporary portfolio to do the job while I find time to work on my comprehensive interdisciplinary portfolio. It is fully responsive to device screens of all sizes, and fully supports the user's dark and light mode preferences. It is made **completely from scratch** in Next.js. The backgrounds were created in Figma.
+
+## Tech Stack
+
+### Frontend
+
+* tailwindcss
+* framer-motion
+* react-icons
+
+### Backend
+
+* Node.js
+* Firebase Cloud Firestore
+
+* Next.js framework (although this is a SPA, Next.js provides a number of features beyond plain React that are still beneficial for it, especially if this website were to expand in the future)
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,8 +6,9 @@ import { motion } from "framer-motion";
 import { BsGithub } from "react-icons/bs";
 import { TbBrandNextjs, TbBrandFirebase } from "react-icons/tb";
 import { HiArrowUpRight } from "react-icons/hi2";
+import { UrlObject } from "url";
 
-const Card = () => {
+const Card = (props: {label: string, children: string, href: string | UrlObject}) => {
   return (
     <motion.div
       className="flex flex-col space-y-1 backdrop-blur-md rounded-xl bg-neutral-900 dark:bg-neutral-200 dark:bg-opacity-10 bg-opacity-10 p-2 lg:p-4 m-2"
@@ -17,14 +18,13 @@ const Card = () => {
       }}
     >
       <Link
-        href="/"
+        href={props.href}
         className="flex flex-col justify-between space-y-1 w-60 h-48"
       >
         <div className="flex flex-col space-y-1">
-          <CardLabel>snapshot</CardLabel>
+          <CardLabel>{props.label}</CardLabel>
           <CardDescription>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            {props.children}
           </CardDescription>
         </div>
         <div className="flex flex-row items-center justify-between">
